e67ff9f57692034447b6e8fa35fed12311f41871
ash162011 <83339498+ash162011@users.noreply.github.com>
2021-12-17 19:58:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	coin = loadSound("coin.wav");
+	gamover = loadSound("gameover.wav");
+	mariodie = loadSound("mariodie.wav");
+	kick = loadSound("kick.wav");
+	jump = loadSound("jump.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+	game();
+}
+
+function modelLoaded(){
+	console.log('Model Loaded!');
+}
+
+function gotPoses(results){
+	if(results.length > 0){
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log(results);
+
+	}
+}
